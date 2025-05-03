db67ce356b6d03966e49568113a87ca1d9bf7ce4
a1masssss <almas.issakov.t@gmail.com>
2025-05-03 15:59:53 +0500

    Fix logout functionality to support GET requests

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import SingUpView, CustomLoginView 
+from .views import SingUpView, CustomLoginView, logout_view 
 urlpatterns = [
     path("register/", SingUpView.as_view(), name="register"),
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html", next_page='home'), name="logout"),
+    path("logout/", logout_view, name="logout"),
 ]
